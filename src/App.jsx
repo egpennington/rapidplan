9f9f1ad42418ab9bd0733277101cde63d01290e2
egpennington <egpennington@hotmail.com>
2025-04-30 20:03:09 -0800

    change incident to Incident in name

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Header from './components/Header'
-import Section1Incident from './components/Section1incident'
+import Section1Incident from './components/Section1Incident'
 import Section3Hazard from './components/Section3hazard'
 
 export default function App() {
