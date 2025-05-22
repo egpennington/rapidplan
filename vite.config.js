7f207397e732c3cb34cf07d18cf6000fc3d79668
egpennington <egpennington@hotmail.com>
2025-05-22 15:51:50 -0800

    add PWA, vite config update, del index mainifest and manifest.js file

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,41 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true
+      },
+      includeAssets: ['favicon.ico', 'android-chrome-512x512.png', 'android-chrome-192x192.png', 'images/splash.png'],
+      manifest: {
+        name: 'RapidPlan – When Seconds Matter',
+        short_name: 'RapidPlan',
+        description: 'Emergency Site Safety & Control Planning App',
+        theme_color: '#004b66',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   server: {
-    port: 3000,
+    port: 3000
   }
 })
